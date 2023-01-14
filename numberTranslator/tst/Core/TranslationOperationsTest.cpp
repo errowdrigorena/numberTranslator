5857c098f4d988a348c54b318ea50b24700601d4
errowdrigorena <xxlgran@gmail.com>
2023-01-14 18:56:22 +0100

    add teens translation

--- numberTranslator/tst/Core/TranslationOperationsTest.cpp
+++ numberTranslator/tst/Core/TranslationOperationsTest.cpp
@@ -52,6 +52,47 @@ INSTANTIATE_TEST_CASE_P(AllUnits, Units_fixture_test, testing::Values(
 		std::make_tuple("apple", '1', false)
 ));
 
+using Word_translationstring_result = std::tuple<std::string, std::string, bool>;
+
+class Teens_fixture_test : public testing::TestWithParam<Word_translationstring_result>
+{
+public:
+};
+
+INSTANTIATE_TEST_CASE_P(AllTeens, Teens_fixture_test, testing::Values(
+		std::make_tuple("a", "", true),
+		std::make_tuple("Eleven", "11", true),
+		std::make_tuple("twelve", "12", true),
+		std::make_tuple("thirTeen", "13",true),
+		std::make_tuple("foUrteeN", "14", true),
+		std::make_tuple("Fifteen", "15", true),
+		std::make_tuple("sixTEEN", "16", true),
+		std::make_tuple("seventeen", "17", true),
+		std::make_tuple("eIghteen", "18", true),
+		std::make_tuple("nineTeen", "19", true),
+		std::make_tuple("Ten", "10", true),
+		std::make_tuple("apple", "1", false)
+));
+
+TEST_P(Teens_fixture_test, WordToTeenTest)
+{
+  auto word_translation_result = GetParam();
+  auto word_to_test = std::get<0>(word_translation_result);
+  auto expected_translation = std::get<1>(word_translation_result);
+  auto is_correct_translation = std::get<2>(word_translation_result);
+
+  auto result = translate_teens(word_to_test);
+
+  if(is_correct_translation)
+  {
+	  ASSERT_EQ(result, expected_translation);
+  }
+  else
+  {
+	  ASSERT_NE(result, expected_translation);
+  }
+}
+
 }
 
 
